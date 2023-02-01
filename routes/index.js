3e27443c7730cba2fc2407c34d7883473327a874
Michel KACOU <Your email>
2023-02-01 16:19:06 +0100

    try fix

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 const fetch = require("node-fetch");
 
+router.get("/", (req, res) => {
+  res.render("index.html");
+});
+
 router.get("/movies", function (req, res) {
   fetch(
     `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}`
